ebe4bfe7db596646a081695388a3419ce2c94074
GAMZE KURAL <gamzekural98@gmail.com>
2021-12-01 21:21:08 +0300

    Add project files.

---
+++ MCD_InnerTypeModelleme/Musteri.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_InnerTypeModelleme
+{
+    public class Musteri
+    {
+        #region Tekil Olarak Kullanılan özellikler(Property)
+        public int MusteriId { get; set; }
+        public string TcKimlikNumarasi { get; set; }
+        public string Isim { get; set; }
+        public string Soyisim { get; set; }
+        public DateTime OlusturmaTarihi { get; set; }
+        public int KullaniciId { get; set; }
+        #endregion
+
+        #region Inner Type gerektiren alanlar(field)
+        public MusteriAdres[] musteriAdresleri;
+        public MusteriIletisimBilgisi[] musteriIletisimBilgileri;
+        public MusteriSiparisBilgisi[] musteriSiparisBilgileri;
+
+        public Musteri()
+        {
+            musteriAdresleri = new MusteriAdres[5];
+            musteriIletisimBilgileri = new MusteriIletisimBilgisi[3];
+            musteriSiparisBilgileri = new MusteriSiparisBilgisi[10];
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ MCD_InnerTypeModelleme/MusteriIletisimBilgisi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_InnerTypeModelleme
+{
+    public class MusteriIletisimBilgisi
+    {
+        public int IletisimTip { get; set; } //717770001:Mobil - 717770002:Sabit Hat
+        public string AlanKodu { get; set; }
+        public string Numara { get; set; }
+        public bool AktifPasif { get; set; }
+
+        public void MusteriIletisimBilgisiTestMetot()
+        {
+            Console.WriteLine("Müşteri İletişim Test Metot");
+        }
+
+    }
+}
